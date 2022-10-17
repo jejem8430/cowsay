3efa485623e3a6e5d9af55a61c1d304e7c1a10d8
jejem8430 <jer.munoz30@gmail.com>
2022-10-17 16:13:33 +0200

    creation

---
+++ exocowsay/index.js
@@ -0,0 +1,14 @@
+const userinfo = require('./information.js');
+
+
+
+const cowSay = require('./node_modules/cowsay');
+console.log(cowSay.say({
+    text: "I'm a moooodule",
+    e: "oO",
+    T: "U "
+}));
+
+
+
+
---
+++ exocowsay/information.js
@@ -0,0 +1,11 @@
+let myuser = {
+    name: "Jerome",
+    campus: "Marseille"
+}
+module.exports = {
+
+    myuser
+}
+console.log(myuser.name, myuser.campus);
+
+
