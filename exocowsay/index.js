5ac9a6bb6ae6e8c517d78998dc9a7a095f30cef8
jejem8430 <jer.munoz30@gmail.com>
2022-10-17 16:43:15 +0200

    rebase

--- exocowsay/index.js
+++ exocowsay/index.js
@@ -4,7 +4,7 @@ const userinfo = require('./information.js');
 
 const cowSay = require('./node_modules/cowsay');
 console.log(cowSay.say({
-    text: "I'm a moooodule",
+    text : "",
     e: "oO",
     T: "U "
 }));
